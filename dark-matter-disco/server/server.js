ad25d3072e11e504bd8bf099043a9f9dd0ecce22
RayMan504 <uraeusmillet@gmail.com>
2019-06-18 17:00:19 -0500

    install socket.io

--- dark-matter-disco/server/server.js
+++ dark-matter-disco/server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
@@ -8,6 +10,8 @@ app.use((req, res, next) => {
 })
 
 app.use(express.static(path.join(__dirname, '../dist/dark-matter-disco')))
+
+
 
 // app.get('/', (req, res) => {
 //     res.sendStatus(200);
