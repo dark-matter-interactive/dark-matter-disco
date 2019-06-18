f4a4d2155c232228391582a9754cfa88659c2b0c
Christopher Huston <chusto2@gmail.com>
2019-06-18 12:33:41 -0500

    added a function to make a get request based on search input

--- dark-matter-disco/src/app/config.service.ts
+++ dark-matter-disco/src/app/config.service.ts
@@ -7,4 +7,10 @@ import { HttpClient } from '@angular/common/http'
 export class ConfigService {
 
   constructor(private http: HttpClient) { }
+
+  searchAudio(val: string) {
+    console.log(val, 'Service');
+    return this.http.get(`https://www.googleapis.com/youtube/v3/search/?key=AIzaSyANj9wF3ST1WPdhrXeQi64o8ApBZa5Ecek&q=${val}&part=snippet&type=video`)
+  }
+
 }
