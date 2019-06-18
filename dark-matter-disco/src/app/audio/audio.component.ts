83baff46db1f71defb9cfdfb37c54848e6f60fa0
Christopher Huston <chusto2@gmail.com>
2019-06-18 12:32:47 -0500

    added a loadAudio function, imported config service to make call to youtube

--- dark-matter-disco/src/app/audio/audio.component.ts
+++ dark-matter-disco/src/app/audio/audio.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { ConfigService } from '../config.service';
 
 @Component({
   selector: 'app-audio',
@@ -8,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
 export class AudioComponent implements OnInit {
 
   val: string = '';
+  videoID: string = '';
+  URL: string = '';
 
-  constructor() { }
-
+  constructor(private http: HttpClient, private configService: ConfigService) { }
+  // https://www.googleapis.com/youtube/v3
   ngOnInit() {
+    // let obs = this.http.get(`https://www.googleapis.com/youtube/v3/search/?key=AIzaSyANj9wF3ST1WPdhrXeQi64o8ApBZa5Ecek&q=${val}&part=snippet&type=video`);
+    // obs.subscribe((response) => console.log(response));
+  }
+
+  loadAudio() {
+    this.configService.searchAudio(this.val).subscribe(data => this.videoID = data.items[0].id.videoId);
+    this.URL = "https://www.youtube.com/embed/" + this.videoID;
+    console.log(this.URL);
   }
 
 }
