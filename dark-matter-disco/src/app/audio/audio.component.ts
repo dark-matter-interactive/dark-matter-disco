534f58a005c255eebc0f6ba404453a7c9ed00a7a
Christopher Huston <chusto2@gmail.com>
2019-06-20 11:13:55 -0500

    added service for websocket and put changesong event to be emitted

--- dark-matter-disco/src/app/audio/audio.component.ts
+++ dark-matter-disco/src/app/audio/audio.component.ts
@@ -3,6 +3,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 import { ConfigService } from '../config.service';
 import { Subscription } from 'rxjs';
+import { LiveSocketService } from '../live-socket.service';
 
 @Component({
   selector: 'app-audio',
@@ -16,10 +17,19 @@ export class AudioComponent implements OnInit {
   vid: string = 'https://www.youtube.com/embed/';
   videoSrc: any
   private audioSubscription: Subscription;
-  constructor(private configService: ConfigService, private sanitizer: DomSanitizer) { }
+  constructor(private configService: ConfigService, private sanitizer: DomSanitizer, private liveSocketService: LiveSocketService) { }
   
+  
+
+
   ngOnInit() {
     this.videoSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.vid + this.videoID);
+
+    this.liveSocketService.on('changeSong', (videoID) => {
+      console.log(videoID);
+      this.videoID = videoID;
+      this.videoSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.vid + this.videoID);
+    })
   }
 
   // function to search youtube then set videoSrc to embed video and render on screen
@@ -28,10 +38,13 @@ export class AudioComponent implements OnInit {
       console.log(response, response.items[0].id.videoId);
       this.videoID = response.items[0].id.videoId;
       this.videoSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.vid + this.videoID);
+      // Using websockets to sync audio for users
+      this.liveSocketService.emit('changeSong', this.videoID);
     }, () => {}, () => {
       console.log('subscription complete');
     });
     console.log(this.videoSrc);
+    
   }
 
   
