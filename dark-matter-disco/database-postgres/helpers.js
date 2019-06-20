849c408c14a5c8751e569bf6111a074256b0e9f8
RayMan504 <uraeusmillet@gmail.com>
2019-06-20 11:29:48 -0500

    "(dfl database) build helper to store logged in Users"

--- dark-matter-disco/database-postgres/helpers.js
+++ dark-matter-disco/database-postgres/helpers.js
@@ -1,7 +1,23 @@
 //require models
 const { User, Friends } = require('./index.js');
 
+const storeUser = (user) => {
+  User.findOrCreate({ 
+    where: { username }, 
+    defaults: {
+      username,
+      starsTotal,
+      status
+    }     
+  }).then((success) => {
+    console.log('stored');
+  })
+};
+
+
+
 const storeFriendRequest = (friend) => {
+  //store all friend requests in database
   Friends.findOrCreate({ 
     defaults: {
       userId,
@@ -13,6 +29,7 @@ const storeFriendRequest = (friend) => {
 }
 
 const acceptFriendRequest = () => {
+  //update status on accepted friend request
   Friends.find({ where: { status: 0 } })
     .on('success', (statusToUpdate) => {
       statusToUpdate.update({
