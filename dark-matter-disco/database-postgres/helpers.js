8164075e9da9a0b890c87f68a699770aff93d12e
RayMan504 <uraeusmillet@gmail.com>
2019-06-20 11:18:53 -0500

    "(dfl database) setup helper to update status on accepted friend request"

--- dark-matter-disco/database-postgres/helpers.js
+++ dark-matter-disco/database-postgres/helpers.js
@@ -12,7 +12,14 @@ const storeFriendRequest = (friend) => {
   });
 }
 
-
+const acceptFriendRequest = () => {
+  Friends.find({ where: { status: 0 } })
+    .on('success', (statusToUpdate) => {
+      statusToUpdate.update({
+        status: 1,
+      });
+    });
+}
 
 
 module.exports.acceptFriendRequest = storeFriendRequest;
