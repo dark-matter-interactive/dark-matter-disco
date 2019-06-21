1a71fc966a246fd87fee62a1d6ae46256de1b309
Christopher Huston <chusto2@gmail.com>
2019-06-21 14:35:46 -0500

    trying to refactor to use youtube iframe api player

--- dark-matter-disco/src/app/audio/audio.component.ts
+++ dark-matter-disco/src/app/audio/audio.component.ts
@@ -13,20 +13,52 @@ import { $ } from 'protractor';
 })
 export class AudioComponent implements OnInit {
 
+  public YT: any;
+  public video: any;
+  public player: any;
+  public reframed: Boolean = false;
+
   val: string = '';
   videoID: string = '';
   vid: string = 'https://www.youtube.com/embed/';
-  autoplay: string = '?rel=0;&autoplay=1&controls=0';
+  autoplay: string = '?rel=0;&autoplay=1&controls=0&enablejsapi=1';
   videoSrc: any
   private audioSubscription: Subscription;
   constructor(private configService: ConfigService, private sanitizer: DomSanitizer, private liveSocketService: LiveSocketService) { }
   
-  
+  init() {
+    let tag = document.createElement('script');
+    tag.src = 'httpa://www.youtube.com/iframe_api';
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
 
 
   ngOnInit() {
+    this.init();
+    this.video = this.videoID
+
+    window['onYouTubeIframeAPIReady'] = (e) => {
+      this.YT = window['YT'];
+      this.reframed = false;
+      this.player = new window['YT'].PLayer('ytplayer', {
+        videoId: this.video,
+        events: {
+          'onStateChange': this.onPlayerStateChange.bind(this),
+          'onError': this.onPlayerError.bind(this),
+          'onReady': (e) => {
+            if (!this.reframed) {
+              this.reframed = true;
+              // reframe(e.target.a);
+            }
+          }
+        }
+      });
+    };
+    
     this.videoSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.vid + this.videoID + this.autoplay);
 
+    // Socket io sending event to change song to other users
     this.liveSocketService.on('changeSong', (videoID) => {
       console.log(videoID);
       this.videoID = videoID;
@@ -49,17 +81,41 @@ export class AudioComponent implements OnInit {
     
   }
 
-  @ViewChild('audioPlayer', {static: false}) audioPlayer: any;
+  onPlayerStateChange(event) {
+    console.log(event);
+    switch (event.data) {
+      case window['YT'].PlayerState.PLAYING:
+        if (this.cleanTime() === 0) {
+          console.log('started ' + this.cleanTime());
+        } else {
+          console.log('playing ' + this.cleanTime())
+        };
+        break;
+      case window['YT'].PlayerState.PAUSED:
+        if (this.player.getDuration() - this.player.getCurrenttime() !== 0) {
+          console.log('paused' + ' @ ' + this.cleanTime());
+        };
+        break;
+      case window['YT'].PlayerState.ENDED:
+        console.log('ended');
+        break;
+    };
+  };
 
-  ngAfterViewInit() {
-    const audioPlayer = this.audioPlayer;
-    // console.dir(audioPlayer.nativeElement.contentWindow.document);
-    // audioPlayer.nativeElement.click(console.log('clicked'))
-  }
-  
-  test() {
-    const audioPlayer = this.audioPlayer;
-    console.dir(audioPlayer.nativeElement.contentWindow);
+  cleanTime() {
+    return Math.round(this.player.getCurrentTime())
+  };
+
+  onPlayerError(event) {
+    switch (event.data) {
+      case 2:
+        console.log('' + this.video)
+        break;
+      case 100:
+        break;
+      case 101 || 150:
+        break;
+    }
   }
   
 }
