9590b5b7b9069c3b285689ca0df71045ff6f6f4c
Jesse Lessinger <jesse.lessinger@gmail.com>
2019-06-28 19:04:29 -0500

    remove skinName from top of screen

--- dark-matter-disco/src/app/toolbar/settings/settings.component.ts
+++ dark-matter-disco/src/app/toolbar/settings/settings.component.ts
@@ -1,37 +1,38 @@
-<<<<<<< HEAD
-import { Component, OnInit, Input } from '@angular/core';
-=======
-import { Component, OnInit, Renderer2 } from '@angular/core';
->>>>>>> 0599b4e0a9d3c98caf68a846c5801be031ae0b81
+import { Component, OnInit, Input, Renderer2 } from "@angular/core";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.css"]
 })
 export class SettingsComponent implements OnInit {
-
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2) {}
 
   @Input() changeSkinName: any;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-<<<<<<< HEAD
-  
-=======
   changeToSpace() {
-    this.renderer.setStyle(document.body, 'background-image', "url('./assets/planet-cartoon-space-background.jpeg')");
+    this.renderer.setStyle(
+      document.body,
+      "background-image",
+      "url('./assets/planet-cartoon-space-background.jpeg')"
+    );
   }
 
   changeToRanch() {
-    this.renderer.setStyle(document.body, 'background-image', "url('./assets/farm-fields-minimal-flat-art-work-style_22350-732.jpg')");
+    this.renderer.setStyle(
+      document.body,
+      "background-image",
+      "url('./assets/farm-fields-minimal-flat-art-work-style_22350-732.jpg')"
+    );
   }
 
   changeToPineapple() {
-    this.renderer.setStyle(document.body, 'background-image', "url('./assets/Pineapple.png')");
+    this.renderer.setStyle(
+      document.body,
+      "background-image",
+      "url('./assets/Pineapple.png')"
+    );
   }
->>>>>>> 0599b4e0a9d3c98caf68a846c5801be031ae0b81
-
 }
