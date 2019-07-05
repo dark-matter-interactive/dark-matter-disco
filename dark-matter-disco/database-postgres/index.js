93e34a626320ad4e6d94f04a19a96960d451b615
RayMan504 <uraeusmillet@gmail.com>
2019-07-05 00:05:41 -0500

    "(achievements) achievements toast dynamically displays achievement title"

--- dark-matter-disco/database-postgres/index.js
+++ dark-matter-disco/database-postgres/index.js
@@ -8,14 +8,17 @@ const Sequelize = require('sequelize');
 
 
 const name = process.env.DB_NAME || 'postgres';
-const username = process.env.DB_USERNAME || 'operationspark';
-const password = process.env.DB_PASSWORD || '';
+const username = process.env.DB_USERNAME || 'root';
+const password = process.env.DB_PASSWORD || 'root';
 
 const sequelize = new Sequelize(name,  username, password, {
   host: process.env.DB_HOSTNAME || 'localhost',
-  dialect: 'postgres'
+  dialect: 'postgres',
 });
 
+// sequelize.sync({
+//   force: true, // Drops info in database for testing
+// });
 
 
 
@@ -57,7 +60,7 @@ const Achievements = sequelize.define('Achievements', {
   },
   name: Sequelize.STRING,
   badgeURL: Sequelize.STRING,
-  starsThreshold: Sequelize.INTEGER
+  starsThreshold: Sequelize.INTEGER,
 });
 
 
